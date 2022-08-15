36cd8fc39d5a31c3dcf0b9809c6bbbe4952ff401
leehonglee10 <leehonglee10@hotmail.com>
2022-08-15 19:05:38 +0900

    added check for blackjack. changed ranks of A, J, Q, K. (ace is only 11, not variable). added functionality to reshuffle deck when it runs out.

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ var makeDeck = function () {
     // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
     // This is an example of a loop without an array.
     var rankCounter = 1;
-    while (rankCounter <= 13) {
+    var maxRank = 13;
+    while (rankCounter <= maxRank) {
       // By default, the card name is the same as rankCounter
       var cardName = rankCounter;
 
@@ -59,6 +60,21 @@ var makeDeck = function () {
 
     // Increment the suit index to iterate over the next suit
     suitIndex += 1;
+  }
+
+  // Loop through deck and set rank of J, Q or K to 10, and ace to 11
+  var cardIndex = 0;
+  while (cardIndex < cardDeck.length) {
+    if (
+      cardDeck[cardIndex].name == "jack" ||
+      cardDeck[cardIndex].name == "queen" ||
+      cardDeck[cardIndex].name == "king"
+    ) {
+      cardDeck[cardIndex].rank = 10;
+    } else if (cardDeck[cardIndex].name == "ace") {
+      cardDeck[cardIndex].rank = 11;
+    }
+    cardIndex += 1;
   }
 
   // Return the completed card deck
@@ -94,22 +110,6 @@ var shuffleCards = function (cardDeck) {
 };
 
 //=============================================
-//deal starting hand func
-var dealStartingHand = function () {
-  //loop to deal two cards per person, starting with player
-  var counter = 0;
-  while (counter < 2) {
-    playerHand.push(deck.pop());
-    dealerHand.push(deck.pop());
-    counter += 1;
-  }
-  console.log("player hand:");
-  console.log(playerHand);
-  console.log("dealer hand:");
-  console.log(dealerHand);
-};
-
-//=============================================
 //total value of hand func
 var checkValueOfHand = function (hand) {
   var valueOfHand = 0;
@@ -122,6 +122,100 @@ var checkValueOfHand = function (hand) {
 };
 
 //=============================================
+//check for blackjack func
+var checkForBlackjack = function (hand) {
+  var blackjack = false;
+  if (hand[0].rank + hand[1].rank == 21) {
+    blackjack = true;
+  }
+  return blackjack;
+};
+
+//=============================================
+//=============================================
+//GAME MODE FUNCS
+//=============================================
+//=============================================
+
+//=============================================
+//deal starting hand func
+var dealStartingHand = function () {
+  //initialise empty output
+  var dealStartingHandOutput = "";
+  //reset hands
+  playerHand = [];
+  dealerHand = [];
+
+  //if deck has less cards than needed to deal, generate a new deck
+  var cardsNeeded = 4;
+  if (deck.length < cardsNeeded) {
+    deck = shuffleCards(makeDeck());
+    dealStartingHandOutput = "Deck reshuffled.<br>";
+    console.log("Deck reshuffled");
+  }
+
+  //loop to deal two cards per person, starting with player
+  var counter = 0;
+  while (counter < 2) {
+    playerHand.push(deck.pop());
+    dealerHand.push(deck.pop());
+    counter += 1;
+  }
+
+  dealStartingHandOutput += "Hand dealt. Click 'Submit' again to see results.";
+  console.log("player hand:");
+  console.log(playerHand);
+  console.log("dealer hand:");
+  console.log(dealerHand);
+
+  return dealStartingHandOutput;
+};
+
+//=============================================
+//final results func
+var finalResults = function () {
+  //initialise empty output value
+  var finalResultsOutput = "";
+
+  //tally score of each player
+  var playerRank = checkValueOfHand(playerHand);
+  var dealerRank = checkValueOfHand(dealerHand);
+
+  //check for blackjack
+  var playerBlackjack = checkForBlackjack(playerHand);
+  var dealerBlackjack = checkForBlackjack(dealerHand);
+
+  //output both players' hands and scores
+  finalResultsOutput = `You drew ${playerHand[0].name} of ${playerHand[0].suit} and ${playerHand[1].name} of ${playerHand[1].suit}.<br>Dealer drew ${dealerHand[0].name} of ${dealerHand[0].suit} and ${dealerHand[1].name} of ${dealerHand[1].suit}.<br>`;
+
+  //split by cases of who has blackjack
+  if (playerBlackjack == true && dealerBlackjack == true) {
+    finalResultsOutput +=
+      "Both you and the dealer have blackjack!<br>It's a draw!";
+  } else if (playerBlackjack == true && dealerBlackjack == false) {
+    finalResultsOutput += "You have blackjack! You win!";
+  } else if (playerBlackjack == false && dealerBlackjack == true) {
+    finalResultsOutput += "Dealer has blackjack! Dealer wins.";
+  }
+  //if no one has blackjack, compare values of hands
+  else {
+    //output both players' hand values
+    finalResultsOutput += `You have ${playerRank} points and dealer has ${dealerRank} points.<br>`;
+    if (playerRank > dealerRank) {
+      //different output depending on who wins
+      finalResultsOutput += "You win!";
+    } else if (playerRank < dealerRank) {
+      finalResultsOutput += "Dealer wins.";
+    } else {
+      finalResultsOutput += "It's a draw.";
+    }
+  }
+
+  finalResultsOutput += "<br> Click 'Submit' to play again!";
+  return finalResultsOutput;
+};
+
+//=============================================
 //=============================================
 //MAIN
 //=============================================
@@ -129,29 +223,32 @@ var checkValueOfHand = function (hand) {
 
 //first, make a deck
 deck = shuffleCards(makeDeck());
+console.log("deck generated");
 
 //main
 var main = function (input) {
   var myOutputValue = "";
   //deal starting hand
   if (mode == "dealStartingHand") {
-    dealStartingHand();
-    myOutputValue = "Hand dealt. Click 'Submit' again to see results.";
-    mode = "finalResults";
-    console.log(mode);
-  } else if (mode == "finalResults") {
-    console.log("final results running");
-    //tally score of each player
-    playerRank = checkValueOfHand(playerHand);
-    dealerRank = checkValueOfHand(dealerHand);
-    myOutputValue = `You drew ${playerHand[0].name} of ${playerHand[0].suit} and ${playerHand[1].name} of ${playerHand[1].suit}.<br>Dealer drew ${dealerHand[0].name} of ${dealerHand[0].suit} and ${dealerHand[0].name} of ${dealerHand[0].suit}.`;
+    //console log mode
+    console.log("========================");
+    console.log("mode:" + mode);
 
-    //different output depending on who wins
-    if (playerRank > dealerRank) {
-      myOutputValue += "<br>You win!";
-    } else {
-      myOutputValue += "<br>Dealer wins.";
-    }
+    //output
+    myOutputValue = dealStartingHand();
+
+    //change mode
+    mode = "finalResults";
+  } else if (mode == "finalResults") {
+    //console log mode
+    console.log("========================");
+    console.log("mode:" + mode);
+
+    //output
+    myOutputValue = finalResults();
+
+    //change mode
+    mode = "dealStartingHand";
   }
   return myOutputValue;
 };
